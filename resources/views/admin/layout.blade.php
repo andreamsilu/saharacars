c12f52ee41b2e2e2054f6b0fc0fca1e06f827fe0
andreamsilu <msiluandrew2020@gmail.com>
2026-05-09 12:42:15 +0300

    Refactor Material Symbols font loading in multiple views by implementing preload and print media loading strategies for improved performance and consistency.

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -10,7 +10,9 @@
 <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700;800&amp;family=Inter:wght@400;500;600;700&amp;display=swap"/>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700;800&amp;family=Inter:wght@400;500;600;700&amp;display=swap" media="print" onload="this.media='all'"/>
 <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Manrope:wght@500;600;700;800&amp;family=Inter:wght@400;500;600;700&amp;display=swap"/></noscript>
-<link rel="stylesheet" href="{{ asset('css/material-symbols-outlined.css') }}"/>
+<link rel="preload" as="style" href="{{ asset('css/material-symbols-outlined.css') }}"/>
+<link rel="stylesheet" href="{{ asset('css/material-symbols-outlined.css') }}" media="print" onload="this.media='all'"/>
+<noscript><link rel="stylesheet" href="{{ asset('css/material-symbols-outlined.css') }}"/></noscript>
     <script id="tailwind-config">
         tailwind.config = {
             darkMode: "class",
